011dad45e486ec6e070163f057ca4e68e5709cb2
Cladius <claudioslopez04@gmail.com>
2026-05-15 16:24:55 -0300

    Update user class with selectedUser variable and add a random index variable

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { DUMMY_USERS } from "../dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: "app-user",
@@ -6,4 +9,8 @@ import { Component } from "@angular/core";
   templateUrl: "./user.html",
   styleUrl: "./user.scss",
 })
-export class User {}
+export class User {
+  // in a class, you could define it as "private" and will be
+  // just accesible within the class and not the template.
+  selectedUser = DUMMY_USERS[randomIndex];
+}
